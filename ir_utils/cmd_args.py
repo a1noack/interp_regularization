8ecf668c5f0bb9bfe48ac81252b0b6f896034bf1
a1noack <adamnoack1@gmail.com>
2020-05-09 17:33:46 -0700

    remove seed loop from training python script

--- ir_utils/cmd_args.py
+++ ir_utils/cmd_args.py
@@ -23,12 +23,13 @@ cmd_opt.add_argument('-nesterov', type=bool, default=True, help='whether nestero
 
 cmd_opt.add_argument('-gpu', type=int, default=0, help='which GPU to use')
 cmd_opt.add_argument('-print_freq', type=int, default=5, help='which epochs to print training stats')
-cmd_opt.add_argument('-n_seeds', type=int, default=5, help='how many models to train with the current settings')
+cmd_opt.add_argument('-seed', type=int, default=42, help='current seed')
 
-cmd_opt.add_argument('-lambda_jr', type=float, default=4e-3, help='jacobian regularization weight value')
-cmd_opt.add_argument('-lambda_ir', type=float, default=4e-3, help='interpretation regularization weight value')
-cmd_opt.add_argument('-lambda_cs', type=float, default=4e-3, help='cosine similarity regularization weight value')
-cmd_opt.add_argument('-lambda_db', type=float, default=4e-3, help='double backpropagation regularization weight value')
+cmd_opt.add_argument('-lambda_jr', type=float, default=0, help='jacobian regularization weight value')
+cmd_opt.add_argument('-lambda_ir', type=float, default=0, help='interpretation regularization weight value')
+cmd_opt.add_argument('-lambda_cs', type=float, default=0, help='cosine similarity regularization weight value')
+cmd_opt.add_argument('-lambda_gm', type=float, default=0, help='simple gradient regularization weight value')
+cmd_opt.add_argument('-lambda_db', type=float, default=0, help='double backpropagation regularization weight value')
 cmd_opt.add_argument('-track_jac', type=int, default=1, help='prints out norm of input output Jacobian during training')
 
 # for adversary
--- train_models.py
+++ train_models.py
@@ -9,7 +9,7 @@ import ir_utils.resnet as resnet
 import ir_utils.simple_models as simple_models
 import ir_utils.wide_resnet as wide_resnet
 from ir_utils.cmd_args import cmd_args as args
-from ir_utils.utils import train_types
+from ir_utils.utils import train_types, get_model_path, instantiate_model
 from ir_utils.loss_utils import frob_norm_jac, norm_im, cos_sim, double_backprop
 import ir_utils.dataloaders as dataloaders
 
@@ -46,7 +46,8 @@ def datapass(dataloader, train=True, adv_test=False):
         elif args.train_type == 'ir':
             loss += (factor * args.lambda_ir) * norm_im(net, samples, labels, target_interps, args.n_classes, args.gpu)
         elif args.train_type == 'cs':
-            loss += (factor * args.lambda_cs) * cos_sim(net, samples, labels, args.gpu)
+            cs_loss, gm_loss = cos_sim(net, samples, labels, args.gpu)
+            loss += factor * ((args.lambda_cs * cs_loss) + (args.lambda_gm * gm_loss))
         elif args.train_type == 'db':
             loss += (1. * args.lambda_db) * double_backprop(net, samples, labels, args.gpu)
         
@@ -59,93 +60,80 @@ def datapass(dataloader, train=True, adv_test=False):
             optimizer.step()
         
         if args.track_jac:
-            avg_jac += frob_norm_jac(net, samples, args.n_classes, args.gpu, for_loss=False)
+            # When performing this calculation for CIFAR-10, 128*10 examples need to be created. This is too much.
+            # So, just take the first 20 examples and use them to calculate the Jacobian
+            avg_jac += frob_norm_jac(net, samples[:20], args.n_classes, args.gpu, for_loss=False)
     
     n_samples = len(dataloader.dataset)
     n_batches = len(dataloader)
                 
     return n_correct / n_samples, total_loss / n_batches, avg_jac / n_batches
 
-# set device
-device = torch.device(args.gpu if torch.cuda.is_available() else 'cpu')
-torch.cuda.set_device(device)
-if device != 'cpu':
-    torch.set_default_tensor_type('torch.cuda.FloatTensor')
+if __name__ == '__main__':
+    # set device
+    device = torch.device(args.gpu if torch.cuda.is_available() else 'cpu')
+    torch.cuda.set_device(device)
+    if device != 'cpu':
+        torch.set_default_tensor_type('torch.cuda.FloatTensor')
 
-# get dataloaders
-if args.dataset == 'CIFAR-10':
-    train_loader, test_loader = dataloaders.cifar10()
-elif args.dataset == 'MNIST':
-    if args.train_type == 'ir':
-        train_loader, test_loader = dataloaders.mnist_interps(smoothgrad=False)
-    else:
-        train_loader, test_loader = dataloaders.mnist()
+    # get dataloaders
+    if args.dataset == 'CIFAR-10':
+        train_loader, test_loader = dataloaders.cifar10()
+    elif args.dataset == 'MNIST':
+        if args.train_type == 'ir':
+            train_loader, test_loader = dataloaders.mnist_interps(smoothgrad=False)
+        else:
+            train_loader, test_loader = dataloaders.mnist()
+
+    if args.norm == 'inf':
+        norm = np.inf
+    elif args.norm == '2':
+        norm = 2
+
+    print(f'Training {args.model_name} model with {train_types[args.train_type]}.\n')
+
+    np.random.seed(args.seed)
+    torch.manual_seed(args.seed)  
     
-if args.norm == 'inf':
-    norm = np.inf
-elif args.norm == '2':
-    norm = 2
-
-print(f'Training {args.n_seeds} {args.model_name} models with {train_types[args.train_type]}.\n')
-
-for model_num in range(args.n_seeds):
-    model_path = f'{args.save_dir}/{args.dataset}/{args.model_name}_{args.train_type}/'
-    if args.train_type == 'ir':
-        model_path = model_path + f'model_ir{args.lambda_ir}_{model_num}'
-    elif args.train_type == 'jr':
-        model_path = model_path + f'model_jr{args.lambda_jr}_{model_num}'
-    elif args.train_type == 'at':
-        model_path = model_path + f'model_pgd{norm}_eps{args.epsilon}_iters{args.iters}_{model_num}'
-    elif args.train_type == 'cs':
-        model_path = model_path + f'model_cs{args.lambda_cs}_{model_num}'
-    elif args.train_type == 'db':
-        model_path = model_path + f'model_db{args.lambda_db}_{model_num}'
-    else:
-        print(f'This is not a supported training type: {args.train_type}.')
-
-    start = time.time()
+    # construct file path to save location and instantiate model
+    model_path = get_model_path(args)
     
-    if args.model_name == 'WRN-28-10':
-        net = wide_resnet.Wide_ResNet(depth=28, widen_factor=10, 
-                                      dropout_rate=args.dropout, 
-                                      num_classes=args.n_classes)
-    elif args.model_name == 'WRN-16-10':
-        net = wide_resnet.Wide_ResNet(depth=16, widen_factor=10, 
-                                      dropout_rate=args.dropout, 
-                                      num_classes=args.n_classes)
-    elif args.model_name == 'SimpleCNN':
-        net = simple_models.SimpleCNN(p_drop=args.dropout)
-        
+    # instantiate model, adversary, optimizer
+    net = instantiate_model(args)
     net.cuda()
-    
+
     adversary = PGDAttack(predict=net, loss_fn=F.cross_entropy, eps=args.epsilon,
           nb_iter=args.iters, eps_iter=args.step_size, rand_init=True,
           clip_min=args.clip_min, clip_max=args.clip_max, ord=norm, targeted=args.targeted)
-    
+
     optimizer = optim.SGD(net.parameters(), lr=args.lr, 
           momentum=args.momentum, 
           weight_decay=args.wd, 
           nesterov=args.nesterov)
-    
-    lr_decayer = CosineAnnealingLR(optimizer, T_max=args.n_epochs, eta_min=0)
-    
-    print(f'TRAINING MODEL {model_num}')
-    print(f'Saving to {model_path}')
 
+    lr_decayer = CosineAnnealingLR(optimizer, T_max=args.n_epochs, eta_min=0)
+
+    # training loop
+    print(f'TRAINING MODEL {args.seed}')
+    print(f'Saving to {model_path}')
+    start = time.time()
     for epoch in range(1, args.n_epochs + 1):
         train_acc, train_loss, norm_jac = datapass(train_loader)
-        
+
         if epoch % args.print_freq == 0:
             test_acc, test_loss, norm_jac = datapass(test_loader, train=False)
             print(f'Epoch #{epoch}:\tTrain loss: {train_loss:.4f}\tTrain acc: {train_acc:.4f}\tTest loss: {test_loss:.4f}\tTest acc: {test_acc:.4f}\tNorm Jac. {norm_jac:.4f}')
         lr_decayer.step()
+    print(f'\nTotal train time: {(time.time()-start)/60:.1f} minutes\n\n')
+
+    # save final model
     try:
         torch.save(net.state_dict(), model_path)
     except OSError:
         print('Error encountered when saving model.')
+
+    # get final test and adversarial accuracy
     adv_acc, _, _ = datapass(test_loader, adv_test=True)
     print(f'Adv. test acc: {adv_acc:.4f}')
     print(f'Attack is {args.attack_type}-{args.iters}, l_{args.norm} norm, epsilon = {args.epsilon}, \
       \n\tstep size = {args.step_size}, iters = {args.iters}\n')
-    
-    print(f'\nTotal train time: {(time.time()-start)/60:.1f} minutes\n\n')
